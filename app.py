ed997801896295b77eb2ead701ec051f4727ad31
whtbdk <68214379+joshuawhtbdk@users.noreply.github.com>
2022-04-09 20:24:48 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/python')
+def python():
+    return render_template('indexp.html')       
+
+@app.route('/generic')
+def generic():
+    return render_template('genericp.html')              
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html')
 
-
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=80, debug=True)
+    app.run(host='222.101.33.71',port=80, debug=True)
